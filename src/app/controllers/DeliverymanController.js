10e52f212422c25832da247d3aaf1c50695e9e5c
Robson Locatelli <rlocatelli9@gmail.com>
2020-02-16 19:54:04 -0300

    change of method in destroy route

--- src/app/controllers/DeliverymanController.js
+++ src/app/controllers/DeliverymanController.js
@@ -95,11 +95,7 @@ class DeliverymanController {
       where: { id },
     }); */
 
-    const deliveryman = await Deliveryman.findOne({
-      where: {
-        id,
-      },
-    });
+    const deliveryman = await Deliveryman.findByPk(id);
 
     if (!deliveryman) {
       return res.status(400).json({ error: 'Deliveryman does not exists' });
